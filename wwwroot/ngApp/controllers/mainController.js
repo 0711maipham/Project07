f63bfa02faefae452a66e0bdd463380782ba0927
Drewski84 <drew@bronnenmail.com>
2017-08-29 15:13:59 -0700

    add main controller funcitons

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -1,5 +1,26 @@
 class MainController {
     constructor() {
-        this.message = "hello from main";
+        this.message = "Calculator";
+        this.input1 = 0;
+        this.input2 = 0;
+        this.results = 0;
+        this.add();
+        this.subtract();
+        this.multiply();
+        this.divide();
     }
+
+add() {
+    this.results = (this.input1 + this.input2);
+}
+subtract() {
+    this.results = (this.input1 - this.input2);
+}
+multiply() {
+    this.results = (this.input1 * this.input2);
+}
+divide() {
+    this.results = (this.input1 / this.input2);
+}
+
 }
