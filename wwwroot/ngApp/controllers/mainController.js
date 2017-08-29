4f6ecb39fd6426ee8c93cb39ac86fbb1d2e946a5
Maiphuong Pham <you@ git config --global user.email maipham@ucdavis.edu>
2017-08-29 16:36:10 -0700

    css

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -5,17 +5,21 @@ class MainController {
         this.input2 = 0;
         this.results = 0;
     }
-add() {
-    this.results = (this.input1 + this.input2);
-}
-subtract() {
-    this.results = (this.input1 - this.input2);
-}
-multiply() {
-    this.results = (this.input1 * this.input2);
-}
-divide() {
-    this.results = (this.input1 / this.input2);
-}
+
+    add() {
+        this.results = (this.input1 + this.input2);
+    }
+
+    subtract() {
+        this.results = (this.input1 - this.input2);
+    }
+
+    multiply() {
+        this.results = (this.input1 * this.input2);
+    }
+    
+    divide() {
+        this.results = (this.input1 / this.input2);
+    }
 
 }
